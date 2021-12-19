81bd655f7373d19e30cbc1ea9b978cafb796fedb
samover <samuel.overloop@gmail.com>
2021-12-19 14:04:01 +0100

    solution 7.2

--- lib/day7.ex
+++ lib/day7.ex
@@ -1,26 +1,32 @@
 defmodule AoC2021.Day7 do
-  def getCheapestFuelAlignment(inputPath) do
+  def getCheapestFuelAlignment(inputPath, rateIncrease) do
     alignments = AoC2021.readFileAndParseFirstLineAsIntegerList(inputPath)
     calculateMostEfficientPath(alignments, [
-      buildPointAndFuelCost(alignments, Enum.min(alignments)),
-      buildPointAndFuelCost(alignments, Enum.max(alignments)),
-    ])
+      buildPointAndFuelCost(alignments, Enum.min(alignments), rateIncrease),
+      buildPointAndFuelCost(alignments, Enum.max(alignments), rateIncrease),
+    ], rateIncrease)
   end
 
-  def calculateMostEfficientPath(alignments, [{ left, leftCost }, { right, rightCost }]) do
-    middlePointFuelCost = buildPointAndFuelCost(alignments, div(left + right, 2))
+  def calculateMostEfficientPath(alignments, [{ left, leftCost }, { right, rightCost }], rateIncrease) do
+    middlePointFuelCost = buildPointAndFuelCost(alignments, div(left + right, 2), rateIncrease)
 
     cond do
       abs(left - right) == 1 -> if leftCost < rightCost, do: leftCost, else: rightCost
-      rightCost > leftCost -> calculateMostEfficientPath(alignments, [{left, leftCost}, middlePointFuelCost])
-      rightCost < leftCost -> calculateMostEfficientPath(alignments, [middlePointFuelCost, {right, rightCost}])
+      rightCost > leftCost -> calculateMostEfficientPath(alignments, [{left, leftCost}, middlePointFuelCost], rateIncrease)
+      rightCost < leftCost -> calculateMostEfficientPath(alignments, [middlePointFuelCost, {right, rightCost}], rateIncrease)
       rightCost == leftCost -> rightCost
     end
   end
 
-  def buildPointAndFuelCost(alignments, point) do {point,calculateFuelCostToPoint(alignments, point)} end
+  def buildPointAndFuelCost(alignments, point, rateIncrease) do {point,calculateFuelCostToPoint(alignments, point, rateIncrease)} end
 
-  def calculateFuelCostToPoint(alignments, point) do
-    Enum.reduce(alignments, 0, fn(pos, fuelCost) -> fuelCost + abs(point - pos) end)
+  def calculateFuelCostToPoint(alignments, point, rateIncrease) do
+    Enum.reduce(alignments, 0, fn(pos, fuelCost) -> 
+      distance = abs(point - pos)
+      endPoint = if rateIncrease, do: distance , else: 1
+      fuelCost + arithmeticSeriesSum(distance, endPoint)
+    end)
   end
+
+  def arithmeticSeriesSum(n, xn) do div(n * (1 + xn), 2) end
 end
--- test/day7_test.exs
+++ test/day7_test.exs
@@ -2,28 +2,19 @@ defmodule Day7Test do
   use ExUnit.Case
   doctest AoC2021.Day7
 
-  test "test input - simulate spawning after 18 day" do
-    assert AoC2021.Day7.getCheapestFuelAlignment(Path.expand("./test/resources/day7_test_input")) == 37
+  test "test input - cheapest fuel alignment with constant fuel rate" do
+    assert AoC2021.Day7.getCheapestFuelAlignment(Path.expand("./test/resources/day7_test_input"), false) == 37
   end
 
-  @tag timeout: :infinity
-  test "real input - simulate spawning after 18 day" do
-    assert AoC2021.Day7.getCheapestFuelAlignment(Path.expand("./test/resources/day7_real_input")) == 336120
+  test "real input - cheapest fuel alignment with constant fuel rate" do
+    assert AoC2021.Day7.getCheapestFuelAlignment(Path.expand("./test/resources/day7_real_input"), false) == 336120
   end
 
-  # test "test input - simulate spawning after 80 days" do
-  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_test_input"), 80) == 5934 
-  # end
-  
-  # test "test input - simulate spawning after 256 days" do
-  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_test_input"), 256) == 26984457539 
-  # end
+  test "test input - cheapest fuel alignment with increasing fuel rate" do
+    assert AoC2021.Day7.getCheapestFuelAlignment(Path.expand("./test/resources/day7_test_input"), true) == 168
+  end
 
-  # test "real input - simulate spawning after 80 days" do
-  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_real_input"), 80) == 359344 
-  # end
-
-  # test "real input - simulate spawning after 256 days" do
-  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_real_input"), 256) == 1629570219571 
-  # end
+  test "real input - cheapest fuel alignment with increasing fuel rate" do
+    assert AoC2021.Day7.getCheapestFuelAlignment(Path.expand("./test/resources/day7_real_input"), true) == 96864235
+  end
 end
