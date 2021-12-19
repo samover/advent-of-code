8570e39e55ff3962e8abe1f4ca1254f9d91d1f61
samover <samuel.overloop@gmail.com>
2021-12-19 11:44:12 +0100

    prepare for day 7

---
+++ test/day7_test.exs
@@ -0,0 +1,24 @@
+defmodule Day7Test do
+  use ExUnit.Case
+  doctest AoC2021.Day7
+
+  # test "test input - simulate spawning after 18 day" do
+  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_test_input"), 18) == 26
+  # end
+
+  # test "test input - simulate spawning after 80 days" do
+  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_test_input"), 80) == 5934 
+  # end
+  
+  # test "test input - simulate spawning after 256 days" do
+  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_test_input"), 256) == 26984457539 
+  # end
+
+  # test "real input - simulate spawning after 80 days" do
+  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_real_input"), 80) == 359344 
+  # end
+
+  # test "real input - simulate spawning after 256 days" do
+  #   assert AoC2021.Day7.lanternFishSpawningPredictor(Path.expand("./test/resources/day6_real_input"), 256) == 1629570219571 
+  # end
+end
